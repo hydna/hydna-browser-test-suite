bf561ad4e63a1a8bdcb0acdb245f48e920cef252
Johan Dahlberg <dahlberg.johan@gmail.com>
2013-10-21 15:52:35 +0200

    Added deny-test

---
+++ tests/test-deny.js
@@ -0,0 +1,30 @@
+
+if (typeof test == 'undefined') {
+  alert('This script cannot be included directly.');
+}
+
+var DENY_RESP         = 'DENIED';
+
+var testChannel;
+  
+test.name('deny test');
+test.timeout(5000);
+
+test.setup(function (readyCallback) {
+  testChannel = test.createChannel('open-deny', 'rw+e');
+  test.log('Waiting for channel to open');
+  testChannel.onopen = testChannel.onclose = readyCallback;
+});
+
+test.run(function (doneCallback, e) {
+  if (e.type != "close") {
+    return test.error(new Error('Expected CloseEvent'));
+  }
+  if (e.wasDenied !== true) {
+    return test.error(new Error('Expected wasDenied==true'));
+  }
+  if (e.reason != DENY_RESP) {
+    return test.error(new Error('Bad reason `' + e.reason + '`!=' + DENY_RESP + '`'));
+  }
+  doneCallback();
+});
