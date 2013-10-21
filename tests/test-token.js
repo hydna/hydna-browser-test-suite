9792bd4c3fc1f0d0cf812d609a42b35198502c3b
Johan Dahlberg <dahlberg.johan@gmail.com>
2013-10-21 15:46:00 +0200

    Added "token" test

---
+++ tests/test-token.js
@@ -0,0 +1,30 @@
+
+if (typeof test == 'undefined') {
+  alert('This script cannot be included directly.');
+}
+
+var TOKEN             = 'TOKEN';
+var SIGNAL_RESP       = 'pong';
+
+var testChannel;
+  
+test.name('Signal test');
+test.timeout(5000);
+
+test.setup(function (readyCallback) {
+  testChannel = test.createChannel('test-token?' + TOKEN, 'rw+e');
+  test.log('Waiting for channel to open');
+  testChannel.onopen = readyCallback;
+});
+
+test.run(function (doneCallback, e) {
+  if (e.data != TOKEN) {
+    return test.error(new Error('diff error `' + e.data + '`!=' + TOKEN + '`'));
+  }
+  doneCallback();
+});
+
+test.teardown(function (teardownCallback) {
+  testChannel.onclose = teardownCallback;
+  testChannel.close();
+});
